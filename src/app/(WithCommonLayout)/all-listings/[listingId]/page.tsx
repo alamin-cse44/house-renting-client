2b83ba1c219aa3ccaca23dc274cb93d676908494
alamin-cse44 <alamincse44@gmail.com>
2025-03-03 19:09:16 +0600

    Chore: renting request modal

--- src/app/(WithCommonLayout)/all-listings/[listingId]/page.tsx
+++ src/app/(WithCommonLayout)/all-listings/[listingId]/page.tsx
@@ -1,25 +1,14 @@
 import Gallery from "@/components/modules/listing/Gallery";
-import { Button } from "@/components/ui/button";
+import RentRequestForm from "@/components/modules/listing/RentRequestModal";
 import Shell from "@/components/ui/core/Shell";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
 
 const ListingDetailsPage = async ({
   params,
 }: {
   params: Promise<{ listingId: string }>;
 }) => {
-  
   const { listingId } = await params;
-  
+
   return (
     <Shell className="mt-10">
       <h2>Product details page {listingId}</h2>
@@ -28,35 +17,9 @@ const ListingDetailsPage = async ({
         <Gallery />
 
         {/* Right Side - Form */}
-        <div className="bg-white shadow-lg rounded-lg p-6 border w-full max-w-md">
+        <div className="bg-white shadow-lg rounded-lg p-6 border w-full h-48 max-w-md">
           <h3 className="text-lg font-semibold mb-4">Book This Apartment</h3>
-          {/* <Form {...form}>
-            <form className="space-y-2" onSubmit={form.handleSubmit(onSubmit)}>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="test@gmail.com"
-                        {...field}
-                        value={field.value || ""}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button
-                className="w-full bg-green-600 text-white mt-4"
-                type="submit"
-              >
-                Request Booking
-              </Button>
-            </form>
-          </Form> */}
+          <RentRequestForm />
         </div>
       </div>
     </Shell>
---
+++ src/components/modules/listing/RentRequestModal.tsx
@@ -0,0 +1,153 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import NMImageUploader from "@/components/ui/core/NMImageUploader";
+import ImagePreviewer from "@/components/ui/core/NMImageUploader/ImagePreviewer";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { createCategory } from "@/services/Category";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+const RentRequestModal = () => {
+  const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+  const [imagePreview, setImagePreview] = useState<string[] | []>([]);
+
+  const form = useForm();
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data)
+    if(!data?.isAgree || data?.isAgree == "undefined"){
+      toast.error("Please agree with the terms and conditions");
+      return;
+    }
+    // try {
+    //   console.log("ekhane asche")
+    //   const formData = new FormData();
+    //   formData.append("data", JSON.stringify(data));
+    //   formData.append("icon", imageFiles[0]);
+    //   const res = await createCategory(formData);
+    //   console.log(res);
+    //   if (res?.success) {
+    //     toast.success(res?.message);
+    //   } else {
+    //     toast.error(res?.message);
+    //   }
+    // } catch (err: any) {
+    //   console.error(err);
+    // }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="w-full mt-5">Request For Renting</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Renting Information</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between mt-5">
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        className="h-36 w-72"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {imagePreview.length > 0 ? (
+                <ImagePreviewer
+                  setImageFiles={setImageFiles}
+                  imagePreview={imagePreview}
+                  setImagePreview={setImagePreview}
+                  className="mt-8"
+                />
+              ) : (
+                <div className="mt-8">
+                  <NMImageUploader
+                    setImageFiles={setImageFiles}
+                    setImagePreview={setImagePreview}
+                    label="Upload Icon"
+                  />
+                </div>
+              )}
+            </div>
+
+            <FormField
+              control={form.control}
+              name="isAgree"
+              render={({ field }) => (
+                <FormItem className="mt-5 flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>
+                      I'm accepting the terms and conditions
+                    </FormLabel>
+                    
+                  </div>
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="mt-5 w-full">
+              {isSubmitting ? "Creating...." : "Create"}
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default RentRequestModal;
