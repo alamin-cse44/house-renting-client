52a5cb3d65c7e8b4e4e4fbb894528876278ced97
alamin-cse44 <alamincse44@gmail.com>
2025-03-10 14:21:49 +0600

    Add: updated url in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,7 @@ export const middleware = async (request: NextRequest) => {
     } else {
       return NextResponse.redirect(
         new URL(
-          `http://localhost:3000/login?redirectPath=${pathname}`,
+          `https://house-renting-nine.vercel.app/login?redirectPath=${pathname}`,
           request.url
         )
       );
